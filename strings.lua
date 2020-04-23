589f1a3c78d4506ba1b7261873c820739478f4fa
lolluslollus <lolluslollus@outlook.com>
2020-04-23 18:50:01 +0200

    new option to place platform roofs or not

--- strings.lua
+++ strings.lua
@@ -1,7 +1,15 @@
 function data()
 	return {
 		en = {
-			["_DESC"] = "Modular elevated station",
+			["_DESC"] = [[
+				[h2]Modular elevated passenger station with no frills, available from 1960.[/h2]
+
+This mod will add some "elevated" platforms, stations and tracks into the rail construction menu: use those for elevated stations and the normal ones for normal stations.
+
+If you save a game, then uninstall this mod, you may break your game. To fix it, reinstall the mod and remove its artifacts.
+
+Thanks to Enzojz for luadump.
+			]],
 			["_NAME"] = "Modular elevated station for passengers"
 		},
 	}
