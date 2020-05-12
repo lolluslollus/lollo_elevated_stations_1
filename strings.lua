e04727e1316fb7a503458bf6e90d1ddad9223a28
lolluslollus <lolluslollus@hotmail.com>
2020-05-12 20:17:43 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -2,11 +2,11 @@ function data()
 	return {
 		en = {
 			["_DESC"] = [[
-				[h2]Modular elevated passenger station with no frills, available from 1960.[/h2]
+[h2]Modular elevated passenger station with no frills, available from 1960.[/h2]
 
 This mod will add some "elevated" platforms, stations and tracks into the rail construction menu: use those for elevated stations and the normal ones for normal stations.
 
-Some pics show other mods, like the excellent modular roof by oppiescc (https://steamcommunity.com/sharedfiles/filedetails/?id=1991858933).
+Some pics show other mods, like the excellent modular roof by oppiescc ( https://steamcommunity.com/sharedfiles/filedetails/?id=1991858933 ).
 
 If you save a game, then uninstall this mod, you may break your game. To fix it, reinstall the mod and remove its artifacts.
 
