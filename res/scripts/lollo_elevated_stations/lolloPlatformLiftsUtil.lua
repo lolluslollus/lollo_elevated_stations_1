e4089b74a81cb02b91959cd8b6e84e7422749e13
lolluslollus <lolluslollus@outlook.com>
2020-09-25 20:56:55 +0200

    clean building station names

--- res/scripts/lollo_elevated_stations/lolloPlatformLiftsUtil.lua
+++ res/scripts/lollo_elevated_stations/lolloPlatformLiftsUtil.lua
@@ -172,19 +172,6 @@ return function(height)
                             name = 'walls_below_the_platform',
                             transf = idTransf
                         },
-                        -- {
-                        --     children = {
-                        --         {
-                        --             materials = {'station/rail/era_c/era_c_trainstation_assets.mtl'},
-                        --             mesh = 'station/rail/asset/era_c_station_name/era_c_station_name_lod0.msh',
-                        --             name = 'era_c_station_name',
-                        --             transf = idTransf
-                        --         }
-                        --     },
-                        --     name = 'era_c_station_name_grp',
-                        --     --transf = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, },
-                        --     transf = {2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -2.6, -.1, 1}
-                        -- }
                     },
                     name = 'RootNode',
                     transf = idTransf
