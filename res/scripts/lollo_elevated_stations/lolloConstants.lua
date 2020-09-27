3733a4f0f5aa3d00aa86ff7809aed37f8af98d4e
lolluslollus <lolluslollus@outlook.com>
2020-09-27 15:20:53 +0200

    fix underground clearing

--- res/scripts/lollo_elevated_stations/lolloConstants.lua
+++ res/scripts/lollo_elevated_stations/lolloConstants.lua
@@ -1,6 +1,7 @@
 local availableFrom = 1960
 local defaultHeightIndex = 4
 local defaultPillarsIndex = 1
+local undergroundClearing = 4.4
 local underpassZed = -3
 
 return function()
@@ -8,6 +9,7 @@ return function()
         availableFrom = availableFrom,
         defaultHeightIndex = defaultHeightIndex,
         defaultPillarsIndex = defaultPillarsIndex,
+        undergroundClearing = undergroundClearing,
         underpassZed = underpassZed
     }
 end
