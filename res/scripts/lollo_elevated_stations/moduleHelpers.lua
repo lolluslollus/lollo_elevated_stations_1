7211cbbf59194dc5c2b0c80aa5d8608e3cb8a65e
lolluslollus <lolluslollus@outlook.com>
2020-10-02 23:34:15 +0200

    add railings

--- res/scripts/lollo_elevated_stations/moduleHelpers.lua
+++ res/scripts/lollo_elevated_stations/moduleHelpers.lua
@@ -1,3 +1,4 @@
+local trainstationutil = require 'modules/elevated_trainstationutil'
 local transf = require 'transf'
 local vec3 = require 'vec3'
 
@@ -28,12 +29,39 @@ helpers.addBitsBetweenPlatformAndTrack = function(result, i, j, addModelFn, mode
     end
 end
 
+helpers.isWestNeighbourThere = function(result, i, j, isIncludeTracks)
+    local nextM = result.GetModuleAt(i, j - 1)
+    local ca = (nextM and (nextM.metadata.passenger_platform or nextM.metadata.cargo_platform or (isIncludeTracks and nextM.metadata.track)))
+    or result.connector[1000 * i + 100 * (j - 1) + 7]
+    or helpers.isStationOnHead(result, i, 3)
+
+    return ca
+end
+
+helpers.isEastNeighbourThere = function(result, i, j, isIncludeTracks)
+    local prevM = result.GetModuleAt(i, j + 1)
+    local cb = (prevM and (prevM.metadata.passenger_platform or prevM.metadata.cargo_platform or (isIncludeTracks and prevM.metadata.track)))
+    or result.connector[1000 * i + 100 * (j + 1) + 0]
+    or helpers.isStationOnHead(result, i, 4)
+
+    return cb
+end
+
 helpers.isStationOnHead = function(result, i, side34)
     if not(result) or not(result.occupied) or not(result.occupied[side34]) then return false end
     -- for neighbourIndex = -1, 1 do
     --     if result.occupied[side34][i + neighbourIndex] ~= nil then return true end
     -- end
     if result.occupied[side34][i] ~= nil then return true end
+    return false
+end
+
+helpers.isStationOnSide = function(result, finerThanJ, side12)
+    if not(result) or not(result.occupied) or not(result.occupied[side12]) then return false end
+    -- local _jMin = trainstationutil.stationYMin
+    -- local _jMax = trainstationutil.stationYMax
+    -- local finerThanJ = (j - _jMin) * 4 + 1
+    if result.occupied[side12][finerThanJ] ~= nil then return true end
     return false
 end
 
